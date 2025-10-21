0b3ad6a130f7a15b1054cdfd9bf2fc3643ff3a52
Nils Kevin Körting-Eberhardt <nils@familieeberhardt.de>
2025-10-21 21:11:03 +0200

    - added AppBar to NewsWidget - deleted widget_test.dart
    Signed-off-by: Nils Kevin Körting-Eberhardt <nils@familieeberhardt.de>

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -3,7 +3,7 @@
  *  * (c) 2025 Nils Kevin Koerting-Eberhardt (realEntwickler)
  *  *
  *  * File: GeneratedPluginRegistrant.java
- *  * Last edited on: 21.10.25, 21:03
+ *  * Last edited on: 21.10.25, 21:11
  *  *
  *  * This file is part of the project "SMAYL 2.0".
  *  *
--- lib/pages/NewsWidget.dart
+++ lib/pages/NewsWidget.dart
@@ -3,7 +3,7 @@
  *  * (c) 2025 Nils Kevin Koerting-Eberhardt (realEntwickler)
  *  *
  *  * File: NewsWidget.dart
- *  * Last edited on: 21.10.25, 21:05
+ *  * Last edited on: 21.10.25, 21:11
  *  *
  *  * This file is part of the project "SMAYL 2.0".
  *  *
@@ -29,63 +29,68 @@ class NewsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(10),
-      itemCount: newsList.length,
-      itemBuilder: (context, index) {
-        final NewsItem newsItem = newsList[index];
-        return Card(
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12)),
-          elevation: 3,
-          margin: const EdgeInsets.symmetric(vertical: 8),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
-                child: Text("NewsID: ${newsItem.uniqueId}"),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(newsItem.title, style: Theme
-                        .of(context)
-                        .textTheme
-                        .titleLarge),
-                    const SizedBox(height: 6),
-                    Text(newsItem.description, style: Theme
-                        .of(context)
-                        .textTheme
-                        .bodyMedium),
-                    const SizedBox(height: 6),
-                    Row(
-                      children: [
-                        CircleAvatar(
-                          radius: 12,
-                          child:
-                          Text(newsItem.author[0], style: TextStyle(
-                              color: generateRandomColor(75),
-                              fontSize: 12,
-                              fontWeight: FontWeight.bold)),
-                        ),
-                        SizedBox(width: 6,),
-                        Text('${newsItem.author} • ${newsItem.date
-                            .day}.${newsItem.date.month}.${newsItem.date
-                            .year} • ${newsItem.date.hour}:${newsItem.date
-                            .minute} Uhr', style: const TextStyle(color: Colors
-                            .grey))
-                      ],
-                    ),
-                  ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Neuigkeiten"),
+      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: newsList.length,
+        itemBuilder: (context, index) {
+          final NewsItem newsItem = newsList[index];
+          return Card(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12)),
+            elevation: 3,
+            margin: const EdgeInsets.symmetric(vertical: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
+                  child: Text("NewsID: ${newsItem.uniqueId}"),
                 ),
-              )
-            ],
-          ),
-        );
-      },
+                Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(newsItem.title, style: Theme
+                          .of(context)
+                          .textTheme
+                          .titleLarge),
+                      const SizedBox(height: 6),
+                      Text(newsItem.description, style: Theme
+                          .of(context)
+                          .textTheme
+                          .bodyMedium),
+                      const SizedBox(height: 6),
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 12,
+                            child:
+                            Text(newsItem.author[0], style: TextStyle(
+                                color: generateRandomColor(75),
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold)),
+                          ),
+                          SizedBox(width: 6,),
+                          Text('${newsItem.author} • ${newsItem.date
+                              .day}.${newsItem.date.month}.${newsItem.date
+                              .year} • ${newsItem.date.hour}:${newsItem.date
+                              .minute} Uhr', style: const TextStyle(color: Colors
+                              .grey))
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 
