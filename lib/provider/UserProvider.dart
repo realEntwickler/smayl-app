0536b3bb275cda7465351db01ac94e8f54efef5f
Nils Kevin Körting-Eberhardt <nils@familieeberhardt.de>
2025-10-23 12:35:32 +0200

    added UserProvider
    Signed-off-by: Nils Kevin Körting-Eberhardt <nils@familieeberhardt.de>

---
+++ lib/provider/UserProvider.dart
@@ -0,0 +1,32 @@
+/*
+ *
+ *  * (c) 2025 Nils Kevin Koerting-Eberhardt (realEntwickler)
+ *  *
+ *  * File: UserProvider.dart
+ *  * Created on: 23.10.25, 12:35
+ *  *
+ *  * This file is part of the project "SMAYL 2.0".
+ *  *
+ *  * Licensed under the Creative Commons Attribution - NonCommercial -
+ *  * NoDerivatives 4.0 International License (CC BY-NC-ND 4.0).
+ *  *
+ *  * For more information about this license, please visit:
+ *  * https://creativecommons.org/licenses/by-nc-nd/4.0/
+ *
+ */
+import 'package:smayl/utils/SmaylUser.dart';
+import 'package:uuid/uuid.dart';
+import 'package:uuid/v4.dart';
+
+class UserProvider {
+
+  List<SmaylUser> _registeredUsers = [SmaylUser(UuidV4().generate(), "koertini", "test", "Körting-Eberhardt, Nils", DateTime.now(), true)];
+
+  void addUser(SmaylUser user) {
+    _registeredUsers.add(user);
+  }
+
+  SmaylUser getUserByUsername (String username) {
+    return _registeredUsers.where((element) => element.username.toLowerCase() == username).first;
+  }
+}
